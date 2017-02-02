cf674c6dd4f4783df9ccb281efbb38e8480437b3
Aleph Null <ashaunthomas@siu.edu>
2017-02-02 13:37:59 -0600

    Setting up infrastructure
    (ignore the red square)

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-//HO BOI HERE WE GO!
+//General Variables
+var canvas = document.getElementById("gameSpace");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+//Keyboard stuff
+
+//End Keyboard Stuff
+
+//Game Loop Stuff
+function gameLoop() {
+	//where the magic happens	
+}
+setInterval(draw,10); //run "draw" function every 10 milliseconds (100 fps)
+//End Game Loop Stuff
+
+
