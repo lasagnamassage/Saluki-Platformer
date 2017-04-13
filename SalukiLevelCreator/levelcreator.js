a708c690f10fd1301bd94c43148edf88de246175
Aleph Null <ashaunthomas@siu.edu>
2017-04-13 11:23:40 -0500

    Minor old updates

--- SalukiLevelCreator/levelcreator.js
+++ SalukiLevelCreator/levelcreator.js
@@ -1,54 +1,55 @@
-﻿var rowContainer = "<div class = 'rowContainer'>";
-var box = "<div class = 'box'>"
-var endDiv = "</div>"
-var workSpace = document.getElementById("workSpace");
+﻿var workSpace = document.getElementById("workSpace");
 var heightInput = document.getElementById("height");
 var widthInput = document.getElementById("width");
 var height, width;
-var validInput = /[0-9]/; //regex for nu
-//This function clears the workspace
-function clear() {
-	workSpace.innerHTML = '';
+const MAX = 50;
+var rows = [];
+var Row = function(numColumns) {
+	this.numColumns=numColumns;
+	this.boxes = [];
+	this.createRow = function() {
+		var node = document.createElement("DIV");
+		node.classname="row-container";
+		for (var i = 0; i < numColumns; i++)
+		{
+				var innernode = document.createElement("DIV");
+				innernode.classname="Box";
+				var box = new Box(innernode);
+				this.boxes.push(box);
+				node.appendChild(innernode);
+		}
+		workSpace.appendChild(node);
+	};
 }
-//This function gets data from input
-function getInput(input) {
-	if (input.value.length == 0) {
-		alert(input + "input is empty");
-		return false;
+
+var Box = function(node) {
+		this.node = node;
+}
+function submitInputs() {
+	var h = heightInput.value;
+	var w = widthInput.value;
+	if (isNaN(h) || isNaN(w) || h == "" || w == "")
+		alert("Ya dun goofed");
+	else if (h > MAX || w > MAX || h < 0 || w < 0)
+	{
+			alert("Only accepts positive integers up to " + MAX);
 	}
 	else
 	{
-		for(var i = 0; i < input.value.length; i++)
-		{
-		
-				if (!validInput.test(parseInt(input.value.charAt(i)))) {
-					alert("You've entered invalid input bruh.\nNumbers only");	
-					return false;
-				}
-		}
-		return input.value;
+			var dimensions = [h,w];
+			console.log(dimensions);
+			drawMap(dimensions);
 	}
 }
-//returns height input
-function getHeightInput(){ return getInput(heightInput); }
-//returns width input
-function getWidthInput(){ return getInput(widthInput); }
-//grabs all inputs
-function getInputs() {
-	height = getHeightInput();
-	width = getWidthInput();
-	setWorkSpace(width,height);
-}
 
-function setWorkSpace(width, height) {
-		for (var i = 0; i < height; i++)
-		{
-			workSpace.appendChild(rowContainer);
-			for (var j = 0; j < width; j++)
-			{
-				workSpace.appendChild(box);
-				workSpace.appendChild(endDiv);
-			}
-			workSpace.appedChild(endDiv);
-		}
+function drawMap(dimensions) {
+	var r = dimensions[0];
+	var c = dimensions[1];
+	//draw rows
+	for (var i = 0; i < r; i++)
+	{
+			var row = new Row(c);
+			row.createRow();
+			rows.push(row);
+	}
 }
